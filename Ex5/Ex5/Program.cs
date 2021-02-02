2a7877ba935d9c01713e641094800b7efc7ed1c1
Nemésio Florence Vieira Filho <54808745+NemesioFVF@users.noreply.github.com>
2021-02-02 03:24:04 -0300

    Add files via upload

---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da variável 'a': ");
+            Int32 a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o valor da variável 'b': ");
+            Int32 b = Convert.ToInt32(Console.ReadLine());
+            Int32 c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("Variável 'a' = " + a);
+            Console.WriteLine("Variábel 'b' = " + b);
+            Console.ReadKey();
+        }
+    }
+}
